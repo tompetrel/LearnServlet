c203e36ea916f369a2094041dfd9ed047a7170d2
tompetrel <huutaimarvel@gmail.com>
2020-07-20 16:40:13 +0700

    Student Hibernate

---
+++ student-hibernate/src/main/java/com/softech/student/servlet/InsertStudents.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.softech.student.servlet;
+
+import com.softech.student.controller.StudentsEntityJpaController;
+import com.softech.student.entities.StudentsEntity;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author TAI
+ */
+@WebServlet(name = "InsertStudents", urlPatterns = {"/InsertStudents"})
+public class InsertStudents extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet InsertStudents</title>");
+            out.println("</head>");
+            out.println("<body>");
+
+            String name = request.getParameter("name");
+            String gender = request.getParameter("gender");
+            String phone = request.getParameter("phone");
+            String email = request.getParameter("email");
+            String address = request.getParameter("address");
+
+            StudentsEntity student = new StudentsEntity(0, name, Boolean.parseBoolean(gender), phone, email, address);
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.softech_student-hibernate_war_1.0-SNAPSHOTPU");
+            StudentsEntityJpaController controller = new StudentsEntityJpaController(emf);
+            controller.create(student);
+            out.println("<h1> Student saved </h1>");
+            out.println("<a href='InsertStudents.html'>Insert Another student</a>");
+
+            out.println("</body>");
+            out.println("</html>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
